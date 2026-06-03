3270346e40a40666ed66d05d39cf36ceae64aac5
Kevin <kevinuribe114@gmail.com>
2026-06-03 08:06:46 -0500

    route

--- src/app/api/todolist/[id]/route.ts
+++ src/app/api/todolist/[id]/route.ts
@@ -1,2 +1,20 @@
 import Todolist from "@/src/models/todolist";
 import conectionDB from "@/src/lib/database";
+
+await conectionDB();
+
+export async function GET(
+    request: Request,
+    { params }: { params: Promise<{ dato: string }> },
+) {
+
+    const { dato } = await params;
+
+    const datos = await Todolist.find({ _id: dato });
+
+    return Response.json({
+        data: datos,
+        code: 200,
+        message: "el servicio contesto",
+    });
+}
